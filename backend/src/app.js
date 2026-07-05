900018227b19eb24bb08de5c8bf7cae5e2886415
adithya-sri-chandra-a <asamadithyasrichandra@gmail.com>
2026-07-05 11:51:40 +0530

    debug routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,13 @@ const allowedOrigins = [
 
 app.use(helmet());
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
